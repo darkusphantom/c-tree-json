54498b0af9f683d0efea11cf414f4d1518741fa3
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-09-12 12:49:37 -0400

    feat: json library

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <list>
 #include "ArbolBinario.h"
+#include "json.h"
 
 using namespace std;
 
 int main() {
-	cout << "Doriem";
+	// variables declaration
+	string input = "{ \"hello\": \"world\" }";
+	json::jobject result = json::jobject::parse(input);
+
+	//code
+	string value = result.get("hello");
+	cout << value;
 
 	return 0;
 }
